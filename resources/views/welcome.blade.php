6efbf9de380caeacc71d74605d2ce79dd41450f7
Cooper1004 <jdarcy710@gmail.com>
2023-04-10 12:30:57 +0800

    change something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Cooper</div>
+                <div class="title">Hello Laravel! - by Cooper1004</div>
             </div>
         </div>
     </body>
